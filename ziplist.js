60a220b55f98a7e0484c6977e0597b1d79c6bda6
anamoca <anamoca@hawaii.edu>
2019-02-04 19:02:09 -1000

    Update ziplist.js

--- ziplist.js
+++ ziplist.js
@@ -1,3 +1,4 @@
+//problems part 2
 const testList1 = ['a', 'b', 'c'];
 const testList2 = [1, 2, 3];
 
@@ -11,6 +12,8 @@ function zipList(list1, list2) {
 
 console.log(zipList(testList1, testList2));
 
+//zip takes any number of arrays, result displays interior arrays
+//flatten takes arrays in nested structure and flattens out in single sequence
 function zipListTheSimpleWay(list1, list2) {
   return _.flatten(_.zip(list1, list2));
 }
